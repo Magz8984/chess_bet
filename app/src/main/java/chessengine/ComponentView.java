384c1b5133f47b9184a08784bb7e4989da9c929f
Magz8984 <collinsmuthinja@gmail.com>
2019-03-01 19:59:05 +0300

    Add Component View

--- app/src/main/java/chessengine/ComponentView.java
+++ app/src/main/java/chessengine/ComponentView.java
@@ -5,7 +5,7 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class ComponentView extends View { 
+public class ComponentView extends View {
     public ComponentView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
