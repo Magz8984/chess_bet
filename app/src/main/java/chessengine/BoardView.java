c1033d8683e8d3e68249022cfe9c4e99bed1641c
Magz8984 <collinsmuthinja@gmail.com>
2019-03-03 15:57:07 +0300

    Add Board Views

--- app/src/main/java/chessengine/BoardView.java
+++ app/src/main/java/chessengine/BoardView.java
@@ -1,6 +1,23 @@
 package chessengine;
 
-import View;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.graphics.Canvas;
+
 
 public class BoardView extends View {
+    public BoardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public  BoardView(Context context){
+        super(context);
+
+    }
+
+    @Override
+    protected  void onDraw(Canvas canvas){
+
+    }
 }
--- app/src/main/java/chessengine/Cell.java
+++ app/src/main/java/chessengine/Cell.java
@@ -1,6 +1,10 @@
 package chessengine;
 
+import android.content.Context;
 import android.view.View;
 
 public class Cell extends View {
+    public Cell(Context context) {
+        super(context);
+    }
 }
