5ffa809ebcd39daf75fbce9b6fbcc79fb32f6940
Magz8984 <collinsmuthinja@gmail.com>
2019-03-08 10:20:33 +0300

    Restructure App

--- app/src/main/java/chessbet/app/com/BoardActivity.java
+++ app/src/main/java/chessbet/app/com/BoardActivity.java
@@ -12,7 +12,6 @@ public class BoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
-        ButterKnife.bind(this);
     }
 
 
--- app/src/main/java/chessengine/Cell.java
+++ app/src/main/java/chessengine/Cell.java
@@ -2,9 +2,76 @@ package chessengine;
 
 import android.content.Context;
 import android.view.View;
+import android.util.AttributeSet;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import 	android.graphics.Color;
 
 public class Cell extends View {
-    public Cell(Context context,@Nullable AttributeSet attributeSet) {
-        super(context,attributeSet);
+    private int col;
+    private int row;
+    private boolean isWhite;
+    private Drawable drawable;
+    private Rect rect;
+    Paint paint;
+
+    public Cell(Context contex,AttributeSet atts){
+        super(contex,atts);
+        paint=new Paint();
+        paint.setColor(isWhite() ? Color.WHITE : Color.BLACK); //Set White Or  Black Color;
+        paint.setAntiAlias(true);
+    }
+
+
+    @Override
+    public void onDraw(final  Canvas canvas){
+      canvas.drawRect(rect,paint);
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public boolean isWhite() {
+        return isWhite;
+    }
+
+    public void setWhite(boolean white) {
+        isWhite = white;
+    }
+
+    public void setDrawable(Drawable drawable) {
+        this.drawable = drawable;
+    }
+
+    public Drawable getDrawable() {
+        return drawable;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+
+
+
+        setMeasuredDimension(width, height);
     }
 }
