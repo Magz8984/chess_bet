286e63fd21e8e31774b0b3000c8c2442b914ae32
zecollokaris <collo.kariss@gmail.com>
2019-03-15 11:33:13 +0300

    made a settings button to change activity

--- app/src/main/java/chessbet/app/com/MainActivity.java
+++ app/src/main/java/chessbet/app/com/MainActivity.java
@@ -1,13 +1,33 @@
 package chessbet.app.com;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+//    Buttons
+    private Button mSettingButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSettingButton = (Button) findViewById(R.id.settings);
+        mSettingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettings();
+            }
+        });
+    }
+    public void openSettings(){
+        Intent intent = new Intent(this, AccountActivity.class);
+        startActivity(intent);
     }
 }
