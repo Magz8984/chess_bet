2616ea5afe5df254cbd36ccc773099e18527bd60
Collins Muthinja <c.muthinja@carepay.co.ke>
2019-04-20 10:03:15 +0300

    Add board preference class

--- app/src/main/java/chessengine/BoardPreference.java
+++ app/src/main/java/chessengine/BoardPreference.java
@@ -6,22 +6,23 @@ import android.graphics.Color;
 public class BoardPreference{
     private SharedPreferences.Editor editor;
     private  SharedPreferences sharedPreferences;
-    BoardPreference(SharedPreferences sharedPreferences){
+    public  BoardPreference(SharedPreferences sharedPreferences){
         this.sharedPreferences=sharedPreferences;
         editor=sharedPreferences.edit();
+        editor.apply();
     }
     public int getDark() {
-        return  sharedPreferences.getInt("Dark", Color.GRAY);
+        return  sharedPreferences.getInt("Dark", Color.MAGENTA);
     }
     public int getWhite() {
-        return  sharedPreferences.getInt("White", Color.GRAY);
+        return  sharedPreferences.getInt("White", Color.CYAN);
     }
     public void setDark(int dark) {
         editor.putInt("Dark",dark);
         editor.commit();
     }
     public void setWhite(int white) {
-        editor.putInt("Dark",white);
+        editor.putInt("White",white);
         editor.commit();
     }
 }
