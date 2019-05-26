5fc26e68b65f6cf29957e79c00c559ef9d5e6c78
Collins Muthinja <collinsmuthinja@gmail.com>
2019-05-26 22:50:28 +0300

    Implement new grid menu

--- app/src/main/java/chessbet/app/com/MainActivity.java
+++ app/src/main/java/chessbet/app/com/MainActivity.java
@@ -1,47 +1,21 @@
 package chessbet.app.com;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.GridView;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import chessbet.adapter.com.MenuOptionsAdapter;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
-    @BindView(R.id.BtnPlaytwoplayer) Button btnTwoPlayer;
-    @BindView(R.id.BtnSettings) Button mSettingsButton;
-
+public class MainActivity extends AppCompatActivity{
+    @BindView(R.id.menuItems) GridView menuItems;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        mSettingsButton.setOnClickListener(this);
-        btnTwoPlayer.setOnClickListener(this);
-    }
-
-    //Settings Button Start Activity
-    private void openSettings(){
-        Intent intent = new Intent(this, SettingsActivity.class);
-        startActivity(intent);
-    }
-
-    private void openBoard(){
-        Intent intent=new Intent(this,BoardActivity.class);
-        startActivity(intent);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        if(v.equals(mSettingsButton)){
-            openSettings();
-        }
-        if(v.equals(btnTwoPlayer)){
-            openBoard();
-        }
+        menuItems.setAdapter(new MenuOptionsAdapter(this));
     }
 }
