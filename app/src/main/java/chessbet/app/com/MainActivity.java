2c398b820f14c3de42874b5cc55356d9dca24ba0
Collins Muthinja <collinsmuthinja@gmail.com>
2019-07-28 18:39:03 +0300

    #65 Save existing ui changes

--- app/src/main/java/chessbet/app/com/MainActivity.java
+++ app/src/main/java/chessbet/app/com/MainActivity.java
@@ -1,7 +1,11 @@
 package chessbet.app.com;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.GridView;
 
 
@@ -11,12 +15,20 @@ import chessbet.adapter.MenuOptionsAdapter;
 
 public class MainActivity extends AppCompatActivity{
     @BindView(R.id.menuItems) GridView menuItems;
+    @BindView(R.id.toolBar) Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         menuItems.setAdapter(new MenuOptionsAdapter(this));
+        setSupportActionBar(toolbar);
+        LayoutInflater toolBarInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if(toolBarInflater != null && getSupportActionBar() != null){
+            View view = toolBarInflater.inflate(R.layout.toolbar_view,null);
+            setSupportActionBar(view);
+        }
+
     }
 
     @Override
