22b1e5e76e956a03a299f982c9fd6e7c7f78d12f
Collins Muthinja <collinsmuthinja@gmail.com>
2020-01-03 16:21:05 +0300

    Add game analysis activity

---
+++ app/src/main/java/chessbet/app/com/GameAnalysisActivity.java
@@ -0,0 +1,14 @@
+package chessbet.app.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameAnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_analysis);
+    }
+}
