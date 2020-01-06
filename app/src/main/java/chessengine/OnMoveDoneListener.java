011e0935e52f065b6c48203c7bb5f11685abdc3b
Collins Muthinja <collinsmuthinja@gmail.com>
2020-01-06 16:05:39 +0300

    Add Java Doc

--- app/src/main/java/chessengine/OnMoveDoneListener.java
+++ app/src/main/java/chessengine/OnMoveDoneListener.java
@@ -2,6 +2,11 @@ package chessengine;
 
 import com.chess.engine.player.Player;
 
+/**
+ * Any class using BoardView shall implement this interface to get moves and game states
+ * @see chessbet.app.com.BoardActivity
+ * @see chessbet.app.com.GameAnalysisActivity
+ */
 public interface OnMoveDoneListener{
     void getMoves(MoveLog move);
     void isCheckMate(Player player);
