d7b6bed46462e9a08f5ff0b086a05cc8ae12955b
Collins Muthinja <collinsmuthinja@gmail.com>
2020-06-05 18:12:22 +0300

    Add chess bet app flavours

---
+++ app/src/chessbet/java/app/com/LoginActivity.java
@@ -0,0 +1,17 @@
+package app.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import chessbet.app.com.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_cb);
+    }
+}
