e151d91ffeeee786536de56e305ca34a3581d64f
Collins Muthinja <collinsmuthinja@gmail.com>
2020-06-06 18:55:27 +0300

    Add phone number verification

--- app/src/chessbet/java/chessbet/app/com/MainActivity.java
+++ app/src/chessbet/java/chessbet/app/com/MainActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_cb);
+        setContentView(R.layout.activity_main);
     }
 }
