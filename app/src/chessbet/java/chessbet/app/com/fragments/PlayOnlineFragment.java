271f44e730db6f0dda7f2c97bf139d2a5306b59c
ekbaya <Eliasbaya1223@gmail.com>
2020-06-06 15:30:21 +0300

    Adding Navigation Drawer Activity

---
+++ app/src/chessbet/java/chessbet/app/com/fragments/PlayOnlineFragment.java
@@ -0,0 +1,20 @@
+package chessbet.app.com.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import chessbet.app.com.R;
+
+public class PlayOnlineFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_playonline, container, false);
+        return root;
+    }
+}
