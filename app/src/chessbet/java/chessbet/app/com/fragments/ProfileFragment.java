0dfaf46c65d8e61d858e4464c3c76b8098f57785
ekbaya <Eliasbaya1223@gmail.com>
2020-06-09 22:10:38 +0300

    Refreshing Profile Fragment after user is updated

--- app/src/chessbet/java/chessbet/app/com/fragments/ProfileFragment.java
+++ app/src/chessbet/java/chessbet/app/com/fragments/ProfileFragment.java
@@ -1,7 +1,6 @@
 package chessbet.app.com.fragments;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
@@ -9,6 +8,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -26,6 +26,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
@@ -261,10 +262,20 @@ public class ProfileFragment extends Fragment implements View.OnClickListener,
             this.loader.dismiss();
             if(status){
                 Toasty.success(getContext(), "User Updated", Toasty.LENGTH_LONG).show();
+                reloadActivity();
             } else {
                 Toasty.error(getContext(), "User Not Updated", Toasty.LENGTH_LONG).show();
             }
         }
+    }
+
+    private void reloadActivity() {
+        assert getFragmentManager() != null;
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        if (Build.VERSION.SDK_INT >= 26){
+            ft.setReorderingAllowed(false);
+        }
+        ft.detach(this).attach(this).commit();
     }
 
     @Override
