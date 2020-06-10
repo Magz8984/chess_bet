eac135a75f62ce364420f49aa5308aa1b9a48514
Collins Muthinja <collinsmuthinja@gmail.com>
2020-06-10 14:15:41 +0300

    Add pawn promotion logic for online play

--- app/src/main/java/chessengine/PromotionPieceListener.java
+++ app/src/main/java/chessengine/PromotionPieceListener.java
@@ -3,6 +3,10 @@ package chessengine;
 import com.chess.engine.Move;
 import com.chess.engine.pieces.Piece;
 
+/**
+ * Implemented by promotion views
+ * @see chessbet.app.com.BoardActivity
+ */
 public interface PromotionPieceListener {
     void onPromotionPieceTypeSelected(Piece.PieceType pieceType);
     void onPromotionMoveMade(Move move);
