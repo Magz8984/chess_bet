398bf8781418777d5993bb546e4d26b5a812a525
ekbaya <Eliasbaya1223@gmail.com>
2020-06-11 21:22:38 +0300

    Small fixes

--- app/src/chessbet/java/chessbet/app/com/WithdrawActivity.java
+++ app/src/chessbet/java/chessbet/app/com/WithdrawActivity.java
@@ -14,34 +14,13 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import es.dmoral.toasty.Toasty;
 
-public class WithdrawActivity extends AppCompatActivity implements TextWatcher {
+public class WithdrawActivity extends AppCompatActivity {
     @BindView(R.id.editTextAmount) EditText editTextAmount;
-    @BindView(R.id.txtFee) TextView txtFee;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_withdraw);
         ButterKnife.bind(this);
-        editTextAmount.addTextChangedListener(this);
     }
 
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-        if(!TextUtils.isEmpty(s.toString())) {
-            double amount = Double.parseDouble(s.toString());
-            if (amount < 10) txtFee.setText(getText(R.string.withdrawal_fee));
-            else if (amount < 1000) txtFee.setText(getText(R.string.withdrawal_fee_15));
-            else txtFee.setText(getText(R.string.withdrawal_fee_22));
-        }
-    }
 }
