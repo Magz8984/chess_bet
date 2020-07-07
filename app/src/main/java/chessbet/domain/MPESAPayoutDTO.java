de0e457b2c9b5ba3482093254e6f1bbfe8488120
Collins Muthinja <collinsmuthinja@gmail.com>
2020-07-07 16:53:07 +0300

    Add account holder and recipient to mpesa payout dto

--- app/src/main/java/chessbet/domain/MPESAPayoutDTO.java
+++ app/src/main/java/chessbet/domain/MPESAPayoutDTO.java
@@ -6,26 +6,37 @@ package chessbet.domain;
  */
 public class MPESAPayoutDTO {
     public Amount amount;
-    public String phoneNumber;
+    public String accountHolder;
+    public String recipient;
 
-    public MPESAPayoutDTO(Amount amount, String phoneNumber) {
+    public MPESAPayoutDTO(Amount amount, String accountHolder, String recipient) {
         this.amount = amount;
-        this.phoneNumber = phoneNumber;
+        this.accountHolder = accountHolder;
+        this.recipient = recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getRecipient() {
+        return recipient;
     }
 
     public void setAmount(Amount amount) {
         this.amount = amount;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 
     public Amount getAmount() {
         return amount;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
     }
 }
