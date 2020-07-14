356b400538e3b531e9ab3278d644185414e71c89
Collins Muthinja <collinsmuthinja@gmail.com>
2020-07-14 09:52:34 +0300

    Add transaction view improvement

--- app/src/chessbet/java/chessbet/app/com/TransactionsActivity.java
+++ app/src/chessbet/java/chessbet/app/com/TransactionsActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import java.util.List;
 import java.util.Objects;
@@ -23,6 +24,7 @@ import es.dmoral.toasty.Toasty;
 public class TransactionsActivity extends AppCompatActivity implements View.OnClickListener, PaymentsAPI.TransactionsReceived {
     @BindView(R.id.btnWithdraw) Button btnWithdraw;
     @BindView(R.id.recAllTransactions) RecyclerView recAllTransactions;
+    @BindView(R.id.progressBar) ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +41,7 @@ public class TransactionsActivity extends AppCompatActivity implements View.OnCl
     @Override
     protected void onStart() {
         super.onStart();
+        progressBar.setVisibility(View.VISIBLE);
         String phoneNumber = Objects.requireNonNull(AccountAPI.get().getFirebaseUser().getPhoneNumber()).replace("+", "");
         PaymentsAPI.get().getTransactionsImplementation(phoneNumber, this);
     }
@@ -50,6 +53,7 @@ public class TransactionsActivity extends AppCompatActivity implements View.OnCl
         }
     }
 
+
     private void gotoWithdrawActivity() {
         startActivity(new Intent(new Intent(this, WithdrawActivity.class)));
     }
@@ -57,6 +61,10 @@ public class TransactionsActivity extends AppCompatActivity implements View.OnCl
     @Override
     public void onTransactionsReceived(List<Transaction> transactions) {
         runOnUiThread(() -> {
+            progressBar.setVisibility(View.GONE);
+            if(transactions.isEmpty()) {
+                Toasty.info(this, "No transactions available", Toasty.LENGTH_LONG).show();
+            }
             TransactionsAdapter transactionsAdapter = new TransactionsAdapter(this, transactions);
             // Get transactions
             recAllTransactions.setAdapter(transactionsAdapter);
@@ -65,6 +73,9 @@ public class TransactionsActivity extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onTransactionsReceivedFailure() {
-        runOnUiThread(() -> Toasty.error(this, "Error occurred while fetching transactions",Toasty.LENGTH_LONG).show());
+        runOnUiThread(() -> {
+            progressBar.setVisibility(View.GONE);
+            Toasty.error(this, "Error occurred while fetching transactions",Toasty.LENGTH_LONG).show();
+        });
     }
 }
